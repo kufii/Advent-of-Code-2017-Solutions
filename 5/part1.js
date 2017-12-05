775c55599cda81715669b675936cca2c9770160f
Adrien Pyke <adpyke@gmail.com>
2017-12-05 16:39:24 -0500

    Create part1.js

---
+++ 5/part1.js
@@ -0,0 +1,19 @@
+(function() {
+	'use strict';
+
+	var instructions = document.querySelector('pre').textContent.trim().split('\n').map(function(num) {
+		return parseInt(num);
+	});
+
+	var numJumps = 0;
+
+	var i = 0;
+	while (i >= 0 && i < instructions.length) {
+		var inst = instructions[i];
+		instructions[i]++;
+		i += inst;
+		numJumps++;
+	}
+
+	console.log(numJumps);
+}());
