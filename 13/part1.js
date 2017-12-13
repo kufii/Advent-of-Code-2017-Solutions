4d0aca755b21b15302933e849501026898ba883f
Adrien Pyke <adpyke@gmail.com>
2017-12-13 11:03:45 -0500

    Update part1.js

--- 13/part1.js
+++ 13/part1.js
@@ -9,34 +9,21 @@
 		if (depth > maxDepth) {
 			maxDepth = depth;
 		}
-		layers[depth] = {
-			range: parseInt(match[2]),
-			scanner: 0,
-			scannerDirection: 1
-		};
+		layers[depth] = parseInt(match[2]);
 	});
 
-	var moveScanners = function() {
-		for (var property in layers) {
-			if (layers.hasOwnProperty(property)) {
-				var layer = layers[property];
-				layer.scanner += layer.scannerDirection;
-				if (layer.scanner === 0 || layer.scanner === layer.range - 1) {
-					layer.scannerDirection *= -1;
-				}
-			}
-		}
+	var positionAfterNMoves = function(range, moves) {
+		return (moves) % ((range - 1) * 2);
 	};
 
 	var depth = 0;
 	var severity = 0;
 
 	while (depth <= maxDepth) {
-		var layer = layers[depth];
-		if (layer && layer.scanner === 0) {
-			severity += depth * layer.range;
+		var range = layers[depth];
+		if (range && positionAfterNMoves(range, depth) === 0) {
+			severity += depth * range;
 		}
-		moveScanners();
 		depth++;
 	}
 
