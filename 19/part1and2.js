ce251268f141606a669c9c17b193b1bd375d0182
Adrien Pyke <adpyke@gmail.com>
2017-12-19 09:52:06 -0500

    Create part1and2.js

---
+++ 19/part1and2.js
@@ -0,0 +1,60 @@
+(function() {
+	'use strict';
+
+	var letters = [];
+
+	var maze = document.querySelector('pre').textContent.split('\n').filter(function(line) {
+		return line;
+	}).map(function(line) {
+		return line.split('');
+	});
+
+	var getStartingPoint = function() {
+		var coord = {
+			y: 0
+		};
+
+		for (var x = 0; x < maze[0].length; x++) {
+			if (maze[0][x] === '|') {
+				coord.x = x;
+				break;
+			}
+		}
+
+		return coord;
+	};
+
+	var coord = getStartingPoint();
+	var direction = {
+		x: 0,
+		y: 1
+	};
+
+	var steps = 0;
+
+	while(maze[coord.y][coord.x].trim()) {
+		var square = maze[coord.y][coord.x];
+		if (square.match(/[A-Z]/)) {
+			letters.push(square);
+		} else if (square === '+') {
+			var xAxis = (Math.abs(direction.x) + 1) % 2;
+			var yAxis = (Math.abs(direction.y) + 1) % 2;
+			for (var x = coord.x - xAxis; x <= coord.x + xAxis; x += 2) {
+				for (var y = coord.y - yAxis; y <= coord.y + yAxis; y += 2) {
+					if (y >= 0 && y < maze.length && x >= 0 && x < maze[y].length) {
+						if (maze[y][x].trim()) {
+							direction.x = x - coord.x;
+							direction.y = y - coord.y;
+						}
+					}
+				}
+			}
+		}
+		coord.x += direction.x;
+		coord.y += direction.y;
+		steps++;
+	}
+
+	console.log(letters.join(''));
+	console.log(steps);
+}());
