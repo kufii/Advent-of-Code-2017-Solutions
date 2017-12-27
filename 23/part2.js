0ed499105c9de9a44552ffb971ba82742dc831f6
Adrien Pyke <adpyke@gmail.com>
2017-12-27 10:19:22 -0500

    Update part2.js

--- 23/part2.js
+++ 23/part2.js
@@ -1,29 +1,16 @@
 (function() {
 	'use strict';
 
-	var b = 0,
-		c = 0,
-		d = 0,
-		g = 0,
-		h = 0;
+	var h = 0;
 
-	b = 84;
-	c = b;
-	b *= 100;
-	b += 100000;
-	c = b;
-	c += 17000;
-	do {
-		for (d = 2; d * d <= b; d++) {
+	for (var b = 84 * 100 + 100000, c = b + 17000; b - c !== 0; b += 17) {
+		for (var d = 2; d * d <= b; d++) {
 			if (b % d === 0) {
 				h++;
 				break;
 			}
 		}
-
-		g = b - c;
-		b += 17;
-	} while (g !== 0);
+	}
 
 	console.log(h);
 }());
